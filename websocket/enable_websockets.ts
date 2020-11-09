a108ec070ca572935bcb2511d630275ad3b9d151
Joshua Cline <wwjdtd@gmail.com>
2020-11-08 16:43:45 -0800

    Typo.

--- websocket/enable_websockets.ts
+++ websocket/enable_websockets.ts
@@ -1,8 +1,8 @@
 import type { Middleware, Context } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { WebSocket, acceptable } from 'https://deno.land/std@0.76.0/ws/mod.ts'
-export type WebsocketHandler = (socket: WebSocket, url: URL, headers: Headers) => Promise<void>;
+export type WebSocketHandler = (socket: WebSocket, url: URL, headers: Headers) => Promise<void>;
 
-export function WebSocketMiddleware(handler: WebsocketHandler){
+export function WebSocketMiddleware(handler: WebSocketHandler){
     return async function real_middleware(ctx: Context, next: () => Promise<void>) {
             if(acceptable(ctx.request)) {
             let ws = await ctx.upgrade();
