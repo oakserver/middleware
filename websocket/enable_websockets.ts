82f96da7ea2c8bd792c163f0783259bf66896c27
Joshua Cline <wwjdtd@gmail.com>
2020-11-08 16:42:08 -0800

    Made into a single function for simplicity.

--- websocket/enable_websockets.ts
+++ websocket/enable_websockets.ts
@@ -1,23 +1,14 @@
 import type { Middleware, Context } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { WebSocket, acceptable } from 'https://deno.land/std@0.76.0/ws/mod.ts'
-export type handler = (socket: WebSocket, url: URL, headers: Headers) => Promise<void>;
-export class WebSocketMiddleware {
-    public handler: handler;
+export type WebsocketHandler = (socket: WebSocket, url: URL, headers: Headers) => Promise<void>;
 
-    constructor(handler: handler) {
-        this.handler = handler;
-    }
-
-    private async real_middleware(ctx: Context, next: () => Promise<void>) {
-        if (acceptable(ctx.request)) {
+export function WebSocketMiddleware(handler: WebsocketHandler){
+    return async function real_middleware(ctx: Context, next: () => Promise<void>) {
+            if(acceptable(ctx.request)) {
             let ws = await ctx.upgrade();
-            await this.handler(ws, ctx.request.url, ctx.request.headers);
+            await handler(ws, ctx.request.url, ctx.request.headers);
         } else {
             return await next();
         }
-    }
-
-    public middleware(): Middleware {
-        return async (ctx, next) => { await this.real_middleware(ctx, next) };
-    }
+    };
 }
